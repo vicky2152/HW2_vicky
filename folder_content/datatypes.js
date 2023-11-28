9d685bf5b94d7400734baac878e3c299042061ad
Vicky Naumenko <vickynaumenko@gmail.com>
2023-11-28 18:37:12 +0200

    Homework2 with datatypes and node npm installed

---
+++ folder_content/datatypes.js
@@ -0,0 +1,33 @@
+// Number
+
+let a = 35
+let b = 999
+console.log(a)
+console.log(b)
+
+// Strings
+
+let color = red 
+let Name = Mary
+
+console.log(color)
+
+//Boolean
+
+let cityField = true
+let CountryField = false
+
+//Null
+
+let miles = null
+console.log(miles)
+
+//Undefined
+
+let bus  // Value is undefined, type is undefined
+
+//type of
+
+typeof "Mary"     //  "string"
+typeof 999     //  "number"
+
